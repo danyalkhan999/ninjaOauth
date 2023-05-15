d55a2b6ccddcc89feafc405e29f83e12ca49a64d
Danyal <danyalkhan8271@gmail.com>
2023-05-16 04:03:44 +0530

    remove config from gitignore

---
+++ config/keys.js
@@ -0,0 +1,18 @@
+// add to gitignore
+
+module.exports = {
+  google: {
+    clientID:
+      "584440165618-4e8dph3m7mlvehum4ga28k819sqcd91c.apps.googleusercontent.com",
+    clientSecret: "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",
+  },
+  mongoDB: {
+    URL: "mongodb+srv://danyalkhan8271:Danyal123@cluster0.fdulbci.mongodb.net/ninja-auth?retryWrites=true&w=majority",
+  },
+  jwt: {
+    secretKey: "alhadiisthebest",
+  },
+  session: {
+    cookieKey: "landcruiserbestsuvintheworld",
+  },
+};
